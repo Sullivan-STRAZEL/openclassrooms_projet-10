07d12c2c687fd9f092fe2ddc7d15efb6e5e8036d
Sullivan STRAZEL <strazel.sullivan@hotmail.fr>
2023-12-04 10:11:24 +0100

    (WP Plugins): 'Ocean Elementor Widgets' updated
    (script.js): jQuery function to close the popup converted into JS ES6
    
    (header.php): 'echo' added before 'do_shortcode' function to display the contact form from Contact Form 7 plugin
    
    (style.css): CSS added to modify the contact button in header menu
    
    (WP Plugin - ACF): Return value from Google Maps link field changed from 'Link Array' to 'Link URL'
    
    (WP Plugin - CF7): Mail settings infos modified
    
    (WP Plugin - WP Mail SMTP): Plugin installed and configured with Brevo SMTP

--- wp-content/themes/oceanwp-child-theme-master/scripts/script.js
+++ wp-content/themes/oceanwp-child-theme-master/scripts/script.js
@@ -1,4 +1,13 @@
-// Code pour fermer la popup
-$('.popup-close').click(function(){
-	$(this).parent().hide();
-})
+// Variables
+const popup = document.querySelector('.popup-overlay');
+const popupCloseBtn = document.querySelector('.popup-close');
+
+// FUNCTION - POPUP - Close the popup
+function popupClose() {
+    popup.style.display = 'none';
+}
+
+// EVENT - POPUP - Close the popup when the close button is clicked on
+popupCloseBtn.addEventListener('click', () => {
+    popupClose();
+});
